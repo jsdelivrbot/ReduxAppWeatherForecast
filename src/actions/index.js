7e3244bd6a119b253580541f0770e1eda306500b
ken5scal <kengoscal@gmail.com>
2017-01-17 09:16:52 +0900

    Finish creating action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
@@ -5,7 +7,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 // Action creator that fetch data
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url)
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
