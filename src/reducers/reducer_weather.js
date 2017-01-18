a72dcc8e405cf6db2ce217d95686f13aa2eff671
ken5scal <kengoscal@gmail.com>
2017-01-18 09:19:43 +0900

    First time of using graph within weather_list.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,11 @@
 import { FETCH_WEATHER } from '../actions/index';
 
+// Initial state is array, because we want to display multiple weather in differnt city
 export default function(state = [], action) {
   console.log('Action received', action);
   switch(action.type) {
   case FETCH_WEATHER:
-    // Returning new data array(new + old)
+    // Returning new data array(new + old). We don't want to manipulate existing array.
     // return state.concat([action.payload.data]);
     return [ action.payload.data, ...state ];
   }
