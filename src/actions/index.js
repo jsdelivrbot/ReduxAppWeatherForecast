37c78e5d8e5bd11b4c2546817c4d8850196f84e8
ken5scal <kengoscal@gmail.com>
2017-01-19 13:05:59 +0900

    removed index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = '783ec3f5a03a85f95a2f1069522d172a';
+const API_KEY = '';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
