78c25f164cabccf86003f720407a5e9b50fbced1
ken5scal <kengoscal@gmail.com>
2017-01-20 23:39:25 +0900

    Fix problem

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -5,7 +5,7 @@ export default (props) => {
   console.log("props: " + props.lat, props.lon);
   return (
     <GoogleMapLoader
-      containerElement={ <div stryle={{height: '100%'}} />}
+      containerElement={ <div style={{height: '100%'}} />}
       googleMapElement={
         <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
       }
